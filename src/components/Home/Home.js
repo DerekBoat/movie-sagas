0ea4602e1cc1a0aa3acf7061ee8d91a3717c9cc7
Derek Boat <derek.boat@gmail.com>
2020-03-06 16:10:16 -0600

    got the images and titles to appear

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,7 +13,7 @@ getMovies(){
     return (
       <div className="Home">
         {/* {JSON.stringify(this.props.reduxState.movies)} */}
-        {this.props.reduxState.movies.map(movie=><div key={movie.id}>{movie.title}</div>)}
+        {this.props.reduxState.movies.map(movie=><div key={movie.id}><img src={movie.poster}/>{movie.title}{movie.description}</div>)}
         <br/>
         <br/>
         {/* <div>{this.props.reduxState.searchReducer.data.map(gif=><li key={gif.id}><img alt={gif.url} src={gif.images.original.url} height='100px' /><button className='favoriteButton' name={gif.images.original.url} onClick={this.favoriteButton}>Add to Favs</button></li> )}</div>} */}
